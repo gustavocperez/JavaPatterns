97f4a531bab505bbcf98fe2db324e2fd593d2c96
gustavocperez <85114011+gustavocperez@users.noreply.github.com>
2022-03-21 21:22:31 -0400

    Add files via upload

---
+++ proxypattern/ThirdPartyYoutubeClass.java
@@ -0,0 +1,19 @@
+package br.com.gustavo.proxypattern;
+
+import java.util.HashMap;
+
+public class ThirdPartyYoutubeClass implements ThirdPartyYoutubeLib {
+
+	@Override
+	public HashMap<String, Video> popularVideos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Video getVideo(String videoId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ proxypattern/ThirdPartyYoutubeLib.java
@@ -0,0 +1,11 @@
+package br.com.gustavo.proxypattern;
+
+import java.util.HashMap;
+
+public interface ThirdPartyYoutubeLib {
+
+	HashMap<String, Video> popularVideos();
+	
+	Video getVideo(String videoId);
+	
+}
