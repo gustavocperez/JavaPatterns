791347e87c94d6f92d6faf750c35997a1bdc7435
gustavocperez <85114011+gustavocperez@users.noreply.github.com>
2022-04-04 21:14:15 -0400

    Add files via upload

---
+++ BridgePattern/JavaQuestion.java
@@ -0,0 +1,62 @@
+package br.com.BridgePattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JavaQuestion implements Question {
+	private List<String> questions = new ArrayList<String>();
+	private int current = 0;
+
+	public JavaQuestion() {
+		questions.add("What is class? ");
+		questions.add("What is interface? ");
+		questions.add("What is abstraction? ");
+		questions.add("How Multiple polymorphism s achieved in java ");
+		questions.add("How many types of exception handling are there in java? ");
+		questions.add("Define the keyword final for variable, method, and class in java? ");
+		questions.add("What is abstract class? ");
+		questions.add("What is multi-threading? ");
+	}
+	
+	@Override
+	public void nextQuestion() {
+		if (current <= questions.size() -1)
+			current++;
+		System.out.println(current);
+
+	}
+
+	@Override
+	public void previousQuestion() {
+		if (current > 0)
+			current--;
+
+	}
+
+	@Override
+	public void newQuestion(String quest) {
+		questions.add(quest);
+
+	}
+
+	@Override
+	public void deleteQuestion(String quest) {
+		questions.remove(quest);
+
+	}
+
+	@Override
+	public void displayQuestion() {
+		System.out.println(questions.get(current));
+
+	}
+
+	@Override
+	public void displayAllQuestions() {
+		for (String quest : questions) {
+			System.out.print(quest);
+		}
+
+	}
+
+}
---
+++ BridgePattern/QuestionFormat.java
@@ -0,0 +1,16 @@
+package br.com.BridgePattern;
+
+public class QuestionFormat extends QuestionManager {
+
+	public QuestionFormat(String catalog) {
+		super(catalog);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void displayAll() {
+		System.out.println("\n -----------------------------------------------------------------");
+		super.displayAll();
+		System.out.println("\n -----------------------------------------------------------------");
+	}
+
+}
